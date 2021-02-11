1248481ebca7536466de64f8196e1fb5d28eb76b
amyusup <amyusup26@gmail.com>
2021-02-11 15:48:41 +0700

    setup hosting

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,2 @@
-export const URI = 'http://192.168.43.149:8000/api/v1';
-// export const URI = 'https://tixid-api.herokuapp.com/api/v1';
+// export const URI = 'http://192.168.43.149:8000/api/v1';
+export const URI = 'https://penjualan-api.herokuapp.com/api/v1';
